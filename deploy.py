17dfb4792fc1a6a55097f15d666ed7e80878074a
aagrawal712-ux <aagrawal712@gmail.com>
2026-07-08 13:44:45 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -11,13 +11,13 @@ print("Connecting to Snowflake...")
     echo "Role Exists: ${{ secrets.SNOWFLAKE_ROLE != '' }}"
     echo "Warehouse Exists: ${{ secrets.SNOWFLAKE_WAREHOUSE != '' }}"
 
-conn = snowflake.connector.connect(
+  conn = snowflake.connector.connect(
     account=os.environ["SNOWFLAKE_ACCOUNT"],
     user=os.environ["SNOWFLAKE_USER"],
     password=os.environ["SNOWFLAKE_PASSWORD"],
     warehouse=os.environ["SNOWFLAKE_WAREHOUSE"],
     role=os.environ["SNOWFLAKE_ROLE"]
-)
+    )
 
 cur = conn.cursor()
 
