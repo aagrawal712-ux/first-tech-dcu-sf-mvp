2eab0fcd8667fe2a3c983ebca746c06f962783af
aagrawal712-ux <aagrawal712@gmail.com>
2026-07-08 12:33:06 +0530

    Create deploy.py

---
+++ deploy.py
@@ -0,0 +1,35 @@
+import os
+import snowflake.connector
+
+print("Connecting to Snowflake...")
+
+conn = snowflake.connector.connect(
+    account=os.environ["SNOWFLAKE_ACCOUNT"],
+    user=os.environ["SNOWFLAKE_USER"],
+    password=os.environ["SNOWFLAKE_PASSWORD"],
+    warehouse=os.environ["SNOWFLAKE_WAREHOUSE"],
+    role=os.environ["SNOWFLAKE_ROLE"]
+)
+
+cur = conn.cursor()
+
+try:
+    target_db = os.environ["TARGET_DATABASE"]
+
+    print(f"Deploying to {target_db}")
+
+    cur.execute(f"USE DATABASE {target_db}")
+    cur.execute("USE SCHEMA PUBLIC")
+
+    with open("ddl/customer.sql", "r") as file:
+        sql_script = file.read()
+
+    print("Executing SQL...")
+
+    cur.execute(sql_script)
+
+    print("Deployment Successful!")
+
+finally:
+    cur.close()
+    conn.close()
