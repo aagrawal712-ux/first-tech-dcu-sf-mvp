fe737a0308619920edc05fc6cb14a0cd6afffaee
aagrawal712-ux <aagrawal712@gmail.com>
2026-07-08 14:48:14 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,8 +1,8 @@
 import os
+import glob
 import snowflake.connector
 
 print("Connecting to Snowflake...")
-
 
 conn = snowflake.connector.connect(
     account=os.environ["SNOWFLAKE_ACCOUNT"],
@@ -10,7 +10,7 @@ conn = snowflake.connector.connect(
     password=os.environ["SNOWFLAKE_PASSWORD"],
     warehouse=os.environ["SNOWFLAKE_WAREHOUSE"],
     role=os.environ["SNOWFLAKE_ROLE"]
-    )
+)
 
 cur = conn.cursor()
 
@@ -22,12 +22,25 @@ try:
     cur.execute(f"USE DATABASE {target_db}")
     cur.execute("USE SCHEMA PUBLIC")
 
-    with open("ddl/customer.sql", "r") as file:
-        sql_script = file.read()
+    # Find all TXT files
+    txt_files = glob.glob("**/*.txt", recursive=True)
 
-    print("Executing SQL...")
+    if not txt_files:
+        raise Exception("No .txt files found in repository")
 
-    cur.execute(sql_script)
+    for file_name in sorted(txt_files):
+        print(f"Executing {file_name}")
+
+        with open(file_name, "r", encoding="utf-8") as f:
+            sql_script = f.read().strip()
+
+        if not sql_script:
+            print(f"Skipping empty file: {file_name}")
+            continue
+
+        cur.execute(sql_script)
+
+        print(f"Completed {file_name}")
 
     print("Deployment Successful!")
 
